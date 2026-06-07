666220a4c7b8ae8dcd8756c2a81d91aea62b01af
Daniel Walker <djw2176@columbia.edu>
2026-06-07 18:10:26 -0400

    Functional rhythm block spawning, comments and character pressing detection coming soon

--- restaurant.py
+++ restaurant.py
@@ -1,4 +1,6 @@
 import pygame
+import random
+
 pygame.init()
 
 window_size = (800,600)
@@ -11,8 +13,22 @@ kitchen_image = pygame.transform.scale(kitchen_image, window_size)
 restaurant_image = pygame.image.load('restaurant_background.jpg')
 restaurant_image = pygame.transform.scale(restaurant_image, window_size)
 
+#global variables for screen and cooking state
 current_screen = restaurant_image
+global cooking
+cooking = None
+global cooking_timer
+cooking_timer = 0
+global current_time
+current_time = pygame.time.get_ticks()
+blocks_on_screen = []
+global blocks_made
+blocks_made = 0
+possible_rhythm_chars = ['w', 's', 'r', 't', 'f', 'g', 'h', 'j', 'k', 'l','v', 'z', 'm']
+
 clock = pygame.time.Clock()
+cook_event = pygame.USEREVENT + 1
+
 
 #player class, taking in position and sprite 
 class player(pygame.sprite.Sprite):
@@ -22,7 +38,8 @@ class player(pygame.sprite.Sprite):
             self.speed = 7
             self.image = pygame.image.load('pixel_chef.png')
             self.image = pygame.transform.scale(self.image, (50,150))
-        
+            self.image.set_colorkey((255,255,255)) #makes white background transparent
+
         def move(self):
             keys = pygame.key.get_pressed()
             #left and right movement on left and right keys or a and d keys
@@ -60,15 +77,30 @@ class cooking_appliance():
         self.quality = quality
         if self.type == 'stove':
             self.image = pygame.image.load('pixel_stove.png')
-            self.image = pygame.transform.scale(self.image, (50,150))
+            self.image = pygame.transform.scale(self.image, (50,50))#scales to the height and width of the hitbox
         elif self.type == 'oven':
             self.image = pygame.image.load('pixel_oven.png')
-            self.image = pygame.transform.scale(self.image, (50,150))
+            self.image = pygame.transform.scale(self.image, (50,50))
 
     def stove_cook(self):
-        #rhythm game for cooking on a stove
         print("stove cook")
-         
+        # #rhythm game for cooking on a stove
+        # keys = pygame.key.get_pressed()
+        
+        # #spawn a rhythm block every half second
+        # for i in range(10):
+        #     delta_time = clock.tick(60) 
+        #     if delta_time >= 500: #placeholder for timing, should be based on the quality of the stove and the dish being cooked
+        #         print("block spawn start")
+        #         #randomly generate a character for the rhythm block, for now just using 'a' as a placeholder
+        #         char = 'a'
+        #         block = rhythm_block(char,(250, 750), 100 + i*60, 200) #example of spawning a rhythm block, time window of 2 seconds and x position based on index
+        #         block.draw()
+        #         delta_time = 0
+        #         if keys[pygame.K_a]: #placeholder for checking if the correct key is pressed, should be based on the character of the block
+        #             print("correct key pressed")
+        #             block.delete_block(True) #placeholder for deleting the block if the correct key is pressed, should also check if it's within the time window for scoring purposes
+             
     def oven_cook(self):
         #rhythm game for cooking in an oven
         print("oven cook")
@@ -77,21 +109,35 @@ class cooking_appliance():
         screen.blit(self.image, self.hitbox)
         
 
-class rhythm_block():
-    def __init__(self, character, time_window, x, y):
+class rhythm_block(pygame.sprite.Sprite):
+    def __init__(self, character, time_window, x, y, lifetime = 0):
         #the rhythm blocks for minigame  
+        #self.image = pygame.Surface((50,50), pygame.SRCALPHA) #creates a transparent surface for the block
+        super().__init__()
         self.character = character
         self.time_window = time_window
         self.x = x
         self.y = y
+        font = pygame.font.SysFont('Arial', 30)
+        self.font = font
+        self.lifetime = lifetime
     
     def draw(self):
         #blits a specific circle and ring around that circle
         #position of ring relative to circle should correspond with time in the time window
         # 3 seperate circles - 1 ring, 2 layered for the block itself
-        print("draw")
+        pygame.draw.circle(screen, (185,180,175), (self.x,self.y), 20) #inner circle
+        pygame.draw.circle(screen, (185 ,180,175), (self.x,self.y), 25) #outer ring
+        text = self.font.render(self.character, True, (255, 255, 255))
+        screen.blit(text, (self.x - 10, self.y - 10))
+        self.lifetime += 1
+        #print("draw")
     
-    def delete_block(self):
+    def delete_block(self, correct):
+        if correct:
+            print("should turn green")
+        else:
+            print("should turn red")
         #self deletion - supposedly the kill() method removes all refs to an instance
         self.kill()    
 
@@ -117,7 +163,12 @@ stove1 = cooking_appliance('stove', pygame.Rect(100, 300, 50, 50), 1)
 stove2 = cooking_appliance('stove', pygame.Rect(200, 300, 50, 50), 1)
 stove3 = cooking_appliance('stove', pygame.Rect(300, 300, 50, 50), 1)
 stove4 = cooking_appliance('stove', pygame.Rect(400, 300, 50, 50), 1)
-hitboxes = [stove1.hitbox, stove2.hitbox, stove3.hitbox, stove4.hitbox]
+oven1 = cooking_appliance('oven', pygame.Rect(100, 450, 50, 50), 1)
+oven2 = cooking_appliance('oven', pygame.Rect(200, 450, 50, 50), 1)
+oven3 = cooking_appliance('oven', pygame.Rect(300, 450, 50, 50), 1)
+oven4 = cooking_appliance('oven', pygame.Rect(400, 450, 50, 50), 1)
+appliances = [stove1, stove2, stove3, stove4, oven1, oven2, oven3, oven4]
+#hitboxes = [stove1.hitbox, stove2.hitbox, stove3.hitbox, stove4.hitbox, oven1.hitbox, oven2.hitbox, oven3.hitbox, oven4.hitbox]
 
 while True:
     for event in pygame.event.get():
@@ -127,21 +178,18 @@ while True:
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
                 mouse_pos = event.pos
-                for hitbox in hitboxes:
-                    if hitbox.collidepoint(mouse_pos):
-                        print("clicked on stove")
-                        
-                        
+                for appliance in appliances:
+                    if appliance.hitbox.collidepoint(mouse_pos):
+                        pygame.time.set_timer(cook_event, 500) #placeholder for starting the cooking process, should be based on the quality of the appliance and the dish being cooked
+                        cooking = appliance
+
 
     screen.blit(current_screen, (0,0))
-    #screenChange('restaurant')
-    #screenChange('kitchen')
     plr.move()
     
     plr.draw()
 
     #room change logic
-    print(plr.x)
     if plr.x < 0 and current_screen == restaurant_image:
         #left side of the screen when in the restaurant takes you to the kitchen
         print("should go to kitchen")
@@ -152,15 +200,39 @@ while True:
         print("should go to restaurant")
         screenChange("restaurant")
         plr.x = 25
-    
-
+     
     #drawing everything in kitchen
     if current_screen == kitchen_image:
-       
-        stove1.draw()
-        stove2.draw()
-        stove3.draw()
-        stove4.draw()
+       for appliance in appliances:
+           appliance.draw()
 
+    #cooking logic, cooking global variable used to turn the entire system on/off
+    if cooking != None:
+            if cooking.type == 'stove':
+                if pygame.time.get_ticks() - current_time >= 500:
+                    if blocks_made < 10:
+                        current_time = pygame.time.get_ticks() 
+                        char = random.choice(possible_rhythm_chars)
+                        current_block = rhythm_block(char, (250,750), random.randint(160, 600), random.randint(200, 400)) 
+                        blocks_on_screen.append(current_block)
+                        blocks_made += 1
+                        print("0.5 seconds should have passed")
+                    else:
+                        print("should stop cooking")
+                        cooking = None
+                        blocks_made = 0
+                        blocks_on_screen = [] 
+                        
+            elif cooking.type == 'oven':
+                cooking.oven_cook()       
+
+    for block in blocks_on_screen:
+        block.draw()
+        if block.lifetime/60 >= block.time_window[0]/1000 and block.lifetime/60 <= block.time_window[1]/1000: #placeholder for checking if the block is within the time window, should be based on the quality of the appliance and the dish being cooked
+            print("block is within time window")
+                # keys = pygame.key.get_pressed()
+                # if keys[pygame.K_a]: #placeholder for checking if the correct key is pressed, should be based on the character of the block
+                #     print("correct key pressed")
+                #     block.delete_block(True) #placeholder for deleting the block if the correct key is pressed, should also check if it's within the time window for scoring purposes    
     pygame.display.update()
     clock.tick(60)
