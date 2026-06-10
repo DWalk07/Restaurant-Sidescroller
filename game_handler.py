5cf8de468547da2f026da39a2b77919752285d4b
Daniel Walker <djw2176@columbia.edu>
2026-06-10 17:45:47 -0400

    You can now switch between the food catcher and restaurant portions of the game
    Added
    - a time limit accross both games (haven't implemented what happens at the end)
    - a main menu screen that pops up when you press esc in the restaurant
    - the amount of food you have in the restaurant is determined by how much of each type of food you catch (we can change the carrot thing I have)

--- game_handler.py
+++ game_handler.py
@@ -1,2 +1,35 @@
 #determines when to run the restaurant code or the food-catcher code
 #allows for the restaurant and food catchers to share variables(mainly amount of food you have and money)
+import pygame
+from restaurant import run_restaurant
+from food_catcher import run_food_catcher
+
+pygame.init()
+screen = pygame.display.set_mode((800,600))
+clock = pygame.time.Clock()
+
+time_remaining = 500
+#these variables track the amount of each type of food gathered in the catcher and pass it into the restaurant
+veg = 0
+meat = 0
+fruit = 0
+
+current = "restaurant"
+while current != "quit":
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+        
+        # if event.type == pygame.KEYDOWN:
+        #     if event.key == pygame.K_ESCAPE:
+        #         if on_menu == False:
+        #             on_menu = True
+        #         else:
+        #             on_menu = False
+
+    if current == "restaurant":
+        current, time_remaining = run_restaurant(screen,clock, time_remaining, veg, meat, fruit)
+    elif current == "food_catcher":
+        current, time_remaining, veg, meat, fruit = run_food_catcher(screen,clock, time_remaining)
+   
+
