c9f9cf144057409c5a1767b4956864498bea15b9
dcherfil-svg <dcherfil@hamilton.edu>
2026-06-10 23:31:39 -0400

    Added background music and main menu
    I added a file for the main menu and connected it to the game_handler file. I also made sure the main menu could play music. I found the music online somewhere so it can easily be changed.

--- game_handler.py
+++ game_handler.py
@@ -4,32 +4,35 @@ import pygame
 from restaurant import run_restaurant
 from food_catcher import run_food_catcher
 
-pygame.init()
-screen = pygame.display.set_mode((800,600))
-clock = pygame.time.Clock()
+def run_game_handler(screen, clock, volume=0.5):
+    pygame.mixer.music.set_volume(volume)
+    time_remaining = 500
+    #these variables track the amount of each type of food gathered in the catcher and pass it into the restaurant
+    veg = 0
+    meat = 0
+    fruit = 0
 
-time_remaining = 500
-#these variables track the amount of each type of food gathered in the catcher and pass it into the restaurant
-veg = 0
-meat = 0
-fruit = 0
+    current = "restaurant"
+    while current != "quit":
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            
+            # if event.type == pygame.KEYDOWN:
+            #     if event.key == pygame.K_ESCAPE:
+            #         if on_menu == False:
+            #             on_menu = True
+            #         else:
+            #             on_menu = False
 
-current = "restaurant"
-while current != "quit":
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-        
-        # if event.type == pygame.KEYDOWN:
-        #     if event.key == pygame.K_ESCAPE:
-        #         if on_menu == False:
-        #             on_menu = True
-        #         else:
-        #             on_menu = False
+        if current == "restaurant":
+            current, time_remaining = run_restaurant(screen,clock, time_remaining, veg, meat, fruit)
+        elif current == "food_catcher":
+            current, time_remaining, veg, meat, fruit = run_food_catcher(screen,clock, time_remaining)
+    return "menu"
 
-    if current == "restaurant":
-        current, time_remaining = run_restaurant(screen,clock, time_remaining, veg, meat, fruit)
-    elif current == "food_catcher":
-        current, time_remaining, veg, meat, fruit = run_food_catcher(screen,clock, time_remaining)
-   
-
+if __name__ == "__main__":
+    pygame.init()
+    screen = pygame.display.set_mode((800, 600))
+    clock = pygame.time.Clock()
+    run_game_handler(screen, clock)
